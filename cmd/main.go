092df5f1915f00ef2be440943971dcc7f57a49ef
yahav2305 <15077067+yahav2305@users.noreply.github.com>
2024-04-28 16:27:10 +0000

    Add special case for index.html

--- cmd/main.go
+++ cmd/main.go
@@ -158,6 +158,15 @@ func main() {
 		func(w http.ResponseWriter, r *http.Request) {
 			file_path := filepath.Join(filepath.FromSlash(gen_html_dir_rel_path), r.URL.Path) + web_file_ext
 
+			// Special case: serve index.html in route /
+			if r.URL.Path == "/" {
+				file_path := filepath.Join(filepath.Join(filepath.FromSlash(gen_html_dir_rel_path), "index")) + web_file_ext
+				if _, err := os.Stat(file_path); err == nil {
+					http.ServeFile(w, r, file_path)
+					return
+				}
+			}
+
 			if _, err := os.Stat(file_path); err == nil {
 				http.ServeFile(w, r, file_path)
 				return
