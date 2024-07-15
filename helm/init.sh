d511ec51fbc152b47af8b9d8d54abde8ed3bf454
yahav2305 <15077067+yahav2305@users.noreply.github.com>
2024-07-15 13:20:01 +0000

    Start of loki helm chart Doesn't work yet

--- helm/init.sh
+++ helm/init.sh
@@ -6,6 +6,7 @@ set -e  # Exit when any command exits with a non-zero status
 helm repo add projectcalico https://docs.tigera.io/calico/charts
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo add openebs https://openebs.github.io/openebs
+helm repo add grafana https://grafana.github.io/helm-charts
 
 # Calico
 helm upgrade calico projectcalico/tigera-operator --install --atomic --create-namespace --values ./values/calico.yaml --version v3.28.0 --namespace tigera-operator
@@ -23,3 +24,6 @@ kubectl label namespace default istio-injection=true # Enable istio sidecar inje
 kubectl create -f https://operatorhub.io/install/kiali.yaml # Install Kiali operator
 kubectl apply -f ./resources/kiali-cr.yaml -n istio-system # Install Kiali CR
 kubectl wait --for=condition=Successful kiali kiali -n istio-system # Wait for the Kiali CR to create successfully
+
+# Grafana Loki
+helm upgrade loki grafana/loki --install --atomic --create-namespace --values ./values/loki.yaml --version 6.6.6 --namespace loki
