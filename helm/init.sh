3404a2afb1dd5f05d504def5d57182a68879709d
yahav2305 <15077067+yahav2305@users.noreply.github.com>
2024-07-15 09:11:42 +0000

    Refactor To better see which commands install which resources

--- helm/init.sh
+++ helm/init.sh
@@ -2,14 +2,20 @@
 
 set -e  # Exit when any command exits with a non-zero status
 
-# Repos
+# Helm Repos
 helm repo add projectcalico https://docs.tigera.io/calico/charts
 helm repo add istio https://istio-release.storage.googleapis.com/charts
 helm repo add openebs https://openebs.github.io/openebs
 
-# Releases
+# Calico
 helm upgrade calico projectcalico/tigera-operator --install --atomic --create-namespace --values ./values/calico.yaml --version v3.28.0 --namespace tigera-operator
+
+# OpenEBS
 helm upgrade openebs openebs/openebs --install --atomic --create-namespace --values ./values/openebs.yaml --version 4.1.0 --namespace openebs
+
+# Istio
 helm upgrade istio-base istio/base --install --atomic --create-namespace --values ./values/istio-base.yaml --version 1.22.2 --namespace istio-system
 helm upgrade istio-cni istio/cni --install --atomic --create-namespace --values ./values/istio-cni.yaml --version 1.22.2 --namespace istio-system
 helm upgrade istiod istio/istiod --install --atomic --create-namespace --values ./values/istiod.yaml --version 1.22.2 --namespace istio-system
+kubectl label namespace default istio-injection=true # Enable istio sidecar injection in relevant namespaces before deploying apps in them
+
